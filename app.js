cd4467a51cf2d7640900a75af07dae4647293624
afifeka <37196374+afifeka@users.noreply.github.com>
2018-07-15 09:24:34 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,42 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const settings = require('./settings.json');
+const botconfig = require("./botconfig.json");
+const Discord = require("discord.js");
+const fs = require("fs");
+const bot = new Discord.Client({disableEveryone: true});
+bot.commands = new Discord.Collection();
 
-client.on('ready',() => {
-	console.log('I\'m Online\nI\'m Online');
+fs.readdir("./commands/", (err, files) => {
+
+  if(err) console.log(err);
+  let jsfile = files.filter(f => f.split(".").pop() === "js")
+  if(jsfile.length <= 0){
+    console.log("Couldn't find commands.");
+    return;
+  }
+
+  jsfile.forEach((f, i) =>{
+    let props = require(`./commands/${f}`);
+    console.log(`${f} loaded!`);
+    bot.commands.set(props.help.name, props);
+  });
 });
 
-var prefix = "-"
-client.on('message', message => {
-	if (message.author === client.user) return;
-	if (message.content.startsWith(prefix + 'ping')) {
-		message.channel.sendMessage('pong');
-	}
+bot.on("ready", async () => {
+  console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
+  bot.user.setActivity("Smile is fine!", {type: "PLAYING"});
+
 });
 
-client.login(process.env.BOT_TOKEN);
+bot.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
+
+  let prefix = botconfig.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+  let commandfile = bot.commands.get(cmd.slice(prefix.length));
+  if(commandfile) commandfile.run(bot,message,args);
+
+});
+
+bot.login(process.env.BOT_TOKEN);
